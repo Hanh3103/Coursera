b1c52d38fd3dd4203123607c4b06b37a984682ab
HanhNguyen28 <76269099+HanhNguyen28@users.noreply.github.com>
2021-06-17 13:34:22 +0200

    Update PercolationStats.java

--- PercolationStats.java
+++ PercolationStats.java
@@ -5,10 +5,10 @@ import edu.princeton.cs.algs4.Stopwatch;
 
 public class PercolationStats{
 public static void main(String[] args) {
-final int n = 20;
+        final int n = 20;
         final int t = 20;
         double time = 0;
-  Stopwatch timer = new Stopwatch();
+            Stopwatch timer = new Stopwatch();
             PercolationStats stats = new PercolationStats(n, t);
             time = timer.elapsedTime();
             StdOut.println("mean                    = " + stats.mean());
